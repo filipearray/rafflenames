d1888d74f8e778316ff218a93c5745104170bdd0
Filipe Guimarães <91645260+filipearray@users.noreply.github.com>
2022-01-26 16:21:25 -0300

    Update excalibur.js

--- excalibur.js
+++ excalibur.js
@@ -1,5 +1,5 @@
 function raffle(element){
-    const h1 = document.getElementsByClassName("h1")[0];
+    const h1 = document.getElementById("h1");
     const raffleNames = ["Yohan Keisuke", "Tsukirin Keisuke", "Akeru Taeseki", "Kojojin Endou", "Myendyo Raijin"];
     const namesLength = raffleNames.length;
     const raffledNumber = Math.floor(Math.random() * namesLength);
